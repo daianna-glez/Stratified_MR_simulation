212e44bf3f9a7033d719ec32db62f7368f23def3
daianna-glez <glezdaianna@gmail.com>
2025-10-03 14:40:24 +0100

    Check simulate_confounder()

--- simulations/helper_functions/simulate_confounder.R
+++ simulations/helper_functions/simulate_confounder.R
@@ -1,14 +1,8 @@
 
-#' @title Simulate confounder.
+#' @title Simulate confounder
 #' @description This function simulates a confounder (U) for X and Y across all N individuals.
 #' @param N Total number of individuals. 
 #' @return A numeric vector containing the counfounder value for all N individuals.  
-#' @examples 
-#' \dontrun{
-#' if(interactive()){
-#'  hist(simulate_confounder(100))
-#'  }
-#' }
 #' @rdname simulate_confounder
 #' @export
 
