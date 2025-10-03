eea409e89c52a551f730342326ab499969640cb0
daianna-glez <glezdaianna@gmail.com>
2025-10-03 14:42:03 +0100

    Check simulate_error()

--- simulations/helper_functions/simulate_error.R
+++ simulations/helper_functions/simulate_error.R
@@ -1,14 +1,8 @@
 
-#' @title Simulate errors.
+#' @title Simulate error
 #' @description This function simulates the error term for X and Y across all N individuals.
 #' @param N Total number of individuals. 
 #' @return A numeric vector containing the error for the N individuals.  
-#' @examples 
-#' \dontrun{
-#' if(interactive()){
-#'  hist(simulate_error(100))
-#'  }
-#' }
 #' @rdname simulate_error
 #' @export
 
